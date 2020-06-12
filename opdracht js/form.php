67f3e643e36515135bed98af1b849efe3ad6be91
ahmed <52528651+ahmedalothman955@users.noreply.github.com>
2020-06-12 14:09:50 +0200

    Add files via upload

---
+++ opdracht js/form.php
@@ -0,0 +1,11 @@
+<?php
+if (isset($_POST['name'])&&
+isset($_POST['email'])&&
+isset($_POST['phone'])&&
+isset($_POST['date'])&&
+isset($_POST['message']) ) {
+
+       
+
+}
+?>
---
+++ opdracht js/main.js
@@ -0,0 +1,8 @@
+
+function getInputValue( ){
+    // Selecting the input element and get its value 
+    var inputVal = document.getElementById("name").value;
+    // Displaying the value
+    alert(inputVal);
+}
+
